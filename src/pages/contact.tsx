a64caf7fdc04c772070af3e05eef2cf7d289ce67
linre-90 <juho.lindemark@gmail.com>
2021-12-23 17:10:51 +0200

    Simplified and removed unused states

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
-import { NotReady } from "../components/stateless/NotReady/notReady";
+import React, { useState } from "react";
 import {Link} from "gatsby";
 import {FormValidator, formValidationResult} from "../FormValidator";
 import { NavMenu } from "../components/stateless/navMenu/navMenu"; 
@@ -10,12 +9,10 @@ import {Headline} from "../components/stateless/headline/headline";
 import BrianBot from "../components/statefull/brian/brian";
 
 const Contact:React.FC = ():React.ReactElement => {
-    const componentReady = true;
     const [timeStamp, setTimeStamp] = useState(Date.now());
     const [postingForm, setpostingForm] = useState(false);
     const [messagelen, setMessageLen] = useState(0);
     const [wantToSubmitemail, setwantToSubmitemail] = useState(false);
-    const [formIsValid, setFormIsValid] = useState(false);
 
     const formHandler = new FormValidator(20, 300);
 
@@ -27,10 +24,6 @@ const Contact:React.FC = ():React.ReactElement => {
     const [name, setName] = useState("-1");
     const [acceptedPolicy, setAcceptedPolicy] = useState(false);
 
-    useEffect(() => {
-        validate(false);
-    }, [message, topic, header, email, acceptedPolicy])
-
     const postForm:Function = async () => {
         console.log("TODO sent to backend function");
         let response = await {code: 400}
@@ -40,7 +33,6 @@ const Contact:React.FC = ():React.ReactElement => {
         }
         else if(response.code == 400){ // bad form error
             alert("Server not accepting form. Check form and try again. If the problem continues send email to PLACEHOLDER.")
-            setFormIsValid(false);
             setpostingForm(false);
         }
         else{
@@ -48,7 +40,7 @@ const Contact:React.FC = ():React.ReactElement => {
         }
     }
 
-    const validate:Function = (fromButton:boolean) => {
+    const validate:Function = () => {
         let validation: formValidationResult;
 
         if(!wantToSubmitemail){
@@ -76,166 +68,155 @@ const Contact:React.FC = ():React.ReactElement => {
         }
 
         if(validation.isValid){
-            setFormIsValid(true);
-            if(fromButton){
-                setpostingForm(true);
-                postForm();
-            }
+            setpostingForm(true);
+            postForm();
         }
         else{
-            setFormIsValid(false);
-            if(fromButton){
-                alert("Please check form! " + validation.message);
-            }
+            alert("Please check form! " + validation.message);
         }
     }
  
-    if(componentReady){
-        return (
-            <div>
-                <NavMenu header="Contact" innerHeader="Navigation" navLinks={getAddresses(1)}></NavMenu>
-                <div className="container mt-5 col-12 col-lg-6 col-xl-4">
-                    <div className="row">
-                        <Headline hSize={1} text="Contact form"></Headline>
-                    </div>
-                    <div className="row">
-                        <Headline hSize={4} text="Send me questions, job offerings or just general feedback."></Headline>
-                    </div>
-                    <div className="row">
-                        <Headline hSize={6} text="Please read privacy policy to learn more how your information is handled."></Headline>
-                        <Link className="text-center" to='/privacypolicy'>privacy policy</Link>
-                    </div>
-                    <div className="row">
-                        <div className="col-1"></div>
-                        <div className=" col-10"><hr /></div>
-                        <div className="col-1"></div>
-                    </div>
-
-                    {/* its a trap*/}
-                    <div className="mb-3 invisible">
-                        <label htmlFor="name" className="form-label">name:</label>
-                        <input 
-                            type="text" 
-                            className="form-control col-12" 
-                            placeholder={formHandler.placeHolders.headline} 
-                            required
-                            onKeyUp={(event) => {
-                                const target = event.target as HTMLInputElement;
-                                setName(target.value);
-                            }}
-                        />
-                    </div>
-
-                    {/* Form begins */}
-                    <div className="mb-3">
-                        <label htmlFor="headline" className="form-label">Headline:</label>
-                        <input 
-                            type="text" 
-                            className="form-control col-12" 
-                            placeholder={formHandler.placeHolders.headline} 
-                            required
-                            onKeyUp={(event) => {
-                                const target = event.target as HTMLInputElement;
-                                setHeader(target.value);
-                            }}
-                        />
-                    </div>
-
-                    {/* select topic */}
-                    <div className="mb-3">
-                        <label htmlFor="topic" className="form-label">Topic:</label>
-                        <select className="form-select col-12" id="topic" required>
-                            <option defaultValue={null}>{formHandler.placeHolders.topic}</option>
-                            {
-                                formHandler.getTopics().map((topic) => {
-                                    return <option onClick={() => setTopic(formHandler.getTopics().indexOf(topic))} key={topic} value={formHandler.getTopics().indexOf(topic)}>{topic}</option>
-                                })
-                            }
-                        </select>
-                    </div>
-                    {/* message */}
-                    <div className="mb-0">
-                        <label htmlFor="message" className="form-label">Your message:</label>
-                        <textarea 
-                            onKeyUp={
-                                (event) => {
-                                    const target = event.target as HTMLTextAreaElement;
-                                    setMessageLen(target.value.length);
-                                    setMessage(target.value);
-                                }
-                            }  
-                            className="form-control" 
-                            rows={3} 
-                            placeholder={formHandler.placeHolders.messageArea} 
-                            required minLength={formHandler.minLength} 
-                            maxLength={formHandler.maxLength}>
-                        </textarea>
-                    </div>
-                    <div className="row">
-                        <p className="col-12 text-end"><small>{messagelen}/{formHandler.maxLength}</small></p>
-                    </div>
-
-                    {/* accept privacy policy checkBox */}
-                    <div className="row">
-                        <div className="form-check form-switch switch-mobile d-lg-none">
-                            <input className="form-check-input" type="checkbox" onChange={() => setAcceptedPolicy(!acceptedPolicy)}/>
-                            <label className="form-check-label" htmlFor="acceptPrivacy"><small><u>I have read and accept privacy policy.</u></small></label>
-                        </div>
-                        <div className="form-check form-switch switch-desktop d-none d-lg-block">
-                            <input className="form-check-input" type="checkbox" onChange={() => setAcceptedPolicy(!acceptedPolicy)}/>
-                            <label className="form-check-label" htmlFor="acceptPrivacy"><small><u>I have read and accept privacy policy.</u></small></label>
-                        </div>
-                    </div>
-                    
-                    {/* submit email checkBox */}
-                    <div className="row mb-3">
-                        <div className="form-check form-switch switch-mobile d-lg-none">
-                            <input className="form-check-input" type="checkbox" onChange={() => setwantToSubmitemail(!wantToSubmitemail)}/>
-                            <label className="form-check-label" htmlFor="submitEmail"><small><u>I want to submit my email address for response.</u></small></label>
-                        </div>
-                        <div className="form-check form-switch switch-desktop d-none d-lg-block">
-                            <input className="form-check-input" type="checkbox" onChange={() => setwantToSubmitemail(!wantToSubmitemail)}/>
-                            <label className="form-check-label" htmlFor="submitEmail"><small><u>I want to submit my email address for response.</u></small></label>
-                        </div>
-                    </div>
-
-                    {/* optional email */
-                        wantToSubmitemail &&
-                        <div className="mb-3">
-                            <label htmlFor="email" className="form-label">Email:</label>
-                            <input 
-                                type="email" 
-                                className="form-control col-12" 
-                                placeholder={formHandler.placeHolders.email}
-                                onKeyUp={(event) => {
-                                    const target = event.target as HTMLInputElement;
-                                    setEmail(target.value);
-                                }}
-                            />
-                        </div>
-                    }
-                    {/* Submit button */
-                    !postingForm ?
-                        <div className="row mb-3">
-                            <div className="col-4"></div>
-                            <div className="col-4"></div>
-                            <div className="col-4">
-                                <button type="button" style={{"width":"100%"}} className="btn btn-primary col-4" onClick={() => validate(true)}>Send</button>
-                            </div>
-                        </div>
-                    : 
-                        <div><Spinner size="3x" align="center"></Spinner></div>
-
-                    }
+    return (
+        <div>
+            <NavMenu header="Contact" innerHeader="Navigation" navLinks={getAddresses(1)}></NavMenu>
+            <div className="container mt-5 col-12 col-lg-6 col-xl-4">
+                <div className="row">
+                    <Headline hSize={1} text="Contact form"></Headline>
                 </div>
-                <BrianBot></BrianBot>
-                <Footer></Footer>
+                <div className="row">
+                    <Headline hSize={4} text="Send me questions, job offerings or just general feedback."></Headline>
+                </div>
+                <div className="row">
+                    <Headline hSize={6} text="Please read privacy policy to learn more how your information is handled."></Headline>
+                    <Link className="text-center" to='/privacypolicy'>privacy policy</Link>
+                </div>
+                <div className="row">
+                    <div className="col-1"></div>
+                    <div className=" col-10"><hr /></div>
+                    <div className="col-1"></div>
+                </div>
+
+                {/* its a trap*/}
+                <div className="mb-3 invisible">
+                    <label htmlFor="name" className="form-label">name:</label>
+                    <input 
+                        type="text" 
+                        className="form-control col-12" 
+                        placeholder={formHandler.placeHolders.headline} 
+                        required
+                        onKeyUp={(event) => {
+                            const target = event.target as HTMLInputElement;
+                            setName(target.value);
+                        }}
+                    />
+                </div>
+
+                {/* Form begins */}
+                <div className="mb-3">
+                    <label htmlFor="headline" className="form-label">Headline:</label>
+                    <input 
+                        type="text" 
+                        className="form-control col-12" 
+                        placeholder={formHandler.placeHolders.headline} 
+                        required
+                        onKeyUp={(event) => {
+                            const target = event.target as HTMLInputElement;
+                            setHeader(target.value);
+                        }}
+                    />
+                </div>
+
+                {/* select topic */}
+                <div className="mb-3">
+                    <label htmlFor="topic" className="form-label">Topic:</label>
+                    <select className="form-select col-12" id="topic" required>
+                        <option defaultValue={null}>{formHandler.placeHolders.topic}</option>
+                        {
+                            formHandler.getTopics().map((topic) => {
+                                return <option onClick={() => setTopic(formHandler.getTopics().indexOf(topic))} key={topic} value={formHandler.getTopics().indexOf(topic)}>{topic}</option>
+                            })
+                        }
+                    </select>
+                </div>
+                {/* message */}
+                <div className="mb-0">
+                    <label htmlFor="message" className="form-label">Your message:</label>
+                    <textarea 
+                        onKeyUp={
+                            (event) => {
+                                const target = event.target as HTMLTextAreaElement;
+                                setMessageLen(target.value.length);
+                                setMessage(target.value);
+                            }
+                        }  
+                        className="form-control" 
+                        rows={3} 
+                        placeholder={formHandler.placeHolders.messageArea} 
+                        required minLength={formHandler.minLength} 
+                        maxLength={formHandler.maxLength}>
+                    </textarea>
+                </div>
+                <div className="row">
+                    <p className="col-12 text-end"><small>{messagelen}/{formHandler.maxLength}</small></p>
+                </div>
+
+                {/* accept privacy policy checkBox */}
+                <div className="row">
+                    <div className="form-check form-switch switch-mobile d-lg-none">
+                        <input className="form-check-input" type="checkbox" onChange={() => setAcceptedPolicy(!acceptedPolicy)}/>
+                        <label className="form-check-label" htmlFor="acceptPrivacy"><small><u>I have read and accept privacy policy.</u></small></label>
+                    </div>
+                    <div className="form-check form-switch switch-desktop d-none d-lg-block">
+                        <input className="form-check-input" type="checkbox" onChange={() => setAcceptedPolicy(!acceptedPolicy)}/>
+                        <label className="form-check-label" htmlFor="acceptPrivacy"><small><u>I have read and accept privacy policy.</u></small></label>
+                    </div>
+                </div>
+                
+                {/* submit email checkBox */}
+                <div className="row mb-3">
+                    <div className="form-check form-switch switch-mobile d-lg-none">
+                        <input className="form-check-input" type="checkbox" onChange={() => setwantToSubmitemail(!wantToSubmitemail)}/>
+                        <label className="form-check-label" htmlFor="submitEmail"><small><u>I want to submit my email address for response.</u></small></label>
+                    </div>
+                    <div className="form-check form-switch switch-desktop d-none d-lg-block">
+                        <input className="form-check-input" type="checkbox" onChange={() => setwantToSubmitemail(!wantToSubmitemail)}/>
+                        <label className="form-check-label" htmlFor="submitEmail"><small><u>I want to submit my email address for response.</u></small></label>
+                    </div>
+                </div>
+
+                {/* optional email */
+                    wantToSubmitemail &&
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email:</label>
+                        <input 
+                            type="email" 
+                            className="form-control col-12" 
+                            placeholder={formHandler.placeHolders.email}
+                            onKeyUp={(event) => {
+                                const target = event.target as HTMLInputElement;
+                                setEmail(target.value);
+                            }}
+                        />
+                    </div>
+                }
+                {/* Submit button */
+                !postingForm ?
+                    <div className="row mb-3">
+                        <div className="col-4"></div>
+                        <div className="col-4"></div>
+                        <div className="col-4">
+                            <button type="button" style={{"width":"100%"}} className="btn btn-primary col-4" onClick={() => validate()}>Send</button>
+                        </div>
+                    </div>
+                : 
+                    <div><Spinner size="3x" align="center"></Spinner></div>
+                }
             </div>
-        )
-    }
-    else{
-        return <NotReady pageName="Contact"></NotReady>;
-    }
+            <BrianBot></BrianBot>
+            <Footer></Footer>
+        </div>
+    )
+    
 }
 
 export default Contact;
