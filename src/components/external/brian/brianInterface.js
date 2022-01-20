5c333589bfec26b2aedb1db08d1f7eb3f9195192
linre-90 <juho.lindemark@gmail.com>
2022-01-20 22:27:45 +0200

    removed route

--- src/components/external/brian/brianInterface.js
+++ src/components/external/brian/brianInterface.js
@@ -2,25 +2,6 @@ import React from "react";
 
 // Brians user interaction field.
 const BrianInterface = (props) => {
-    
-    // post searched word to db
-    const logSearchedWord = async (word) => {
-        const payload = { word: word };
-        const response = await fetch(
-            process.env.POST_BRIAN_SEARCH_WORD_URL, {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(payload)
-            }
-        )
-
-        if(response.status !== 200){
-            window.location.href = "/errorPage";
-        }
-    }
-
     return (
         <div className="input-group mb-3">
             <div className="input-group-prepend">
@@ -39,7 +20,6 @@ const BrianInterface = (props) => {
                             name: "This migth be helpfull: " + props.suggestionList[0].name,
                             linkUrl: props.suggestionList[0].url
                         };
-                        logSearchedWord(question.name);
                         props.sendMsg([question, response]);
                         e.target.value = "";
                     }
