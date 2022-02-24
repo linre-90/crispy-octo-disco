a5450bfbbd2612387429d70a961951c2ce8e273b
linre-90 <juho.lindemark@gmail.com>
2022-02-24 15:38:02 +0200

    styled blog reader

--- src/pages/{MarkdownRemark.frontmatter__slug}.tsx
+++ src/pages/{MarkdownRemark.frontmatter__slug}.tsx
@@ -21,27 +21,34 @@ export default function Template({ data }: any): React.ReactElement {
 	return (
 		<div>
 			<CookieBanner></CookieBanner>
-
-			<div className={"container " + theme} id="reader">
-				<div className="form-check form-switch switch-mobile d-lg-none">
-                        <input className="form-check-input" type="checkbox" onChange={() => changeTheme()}/>
-                        <label className="form-check-label">{`Change page theme to ${theme == "dark"? "light" : "dark"}.`}</label>
-                    </div>
-				<hr></hr>
-				<h1>{frontmatter.title}</h1>
-				<p>
-					{
-						frontmatter.tags.map((tag) => {
-							return <span className="badge bg-primary me-2 mb-2">{tag}</span>
-						})
-					}
-				</p>
-				<p><small>{frontmatter.date}</small></p>
-				<div dangerouslySetInnerHTML={{ __html: html }}></div>
-				<div className="pt-5 pb-5">
-					<hr />
+			<div className={theme} id="reader">
+				<div className={"container pt-5"}>
+					<h3>Settings</h3>
+					<div className="form-check form-switch switch-mobile d-lg-none">
+						<input className="form-check-input" type="checkbox" onChange={() => changeTheme()}/>
+						<label className="form-check-label">{`Change page theme to ${theme == "dark"? "light" : "dark"}.`}</label>
+					</div>
+					<div className="form-check form-switch switch-desktop d-none d-lg-block">
+						<input className="form-check-input" type="checkbox" onChange={() => changeTheme()}/>
+						<label className="form-check-label" htmlFor="acceptPrivacy">{`Change page theme to ${theme == "dark"? "light" : "dark"}.`}</label>
+					</div>
+					<div className="border-bottom border-info pt-5"></div>
+					<h1>{frontmatter.title}</h1>
+					<p>
+						{
+							frontmatter.tags.map((tag) => {
+								return <span className="badge bg-warning text-primary me-2 mb-2">{tag}</span>
+							})
+						}
+					</p>
+					<p><small>{frontmatter.date}</small></p>
+					<div dangerouslySetInnerHTML={{ __html: html }}></div>
+					<div className="pt-5 pb-5">
+						<hr />
+					</div>
 				</div>
 			</div>
+			
 		</div>
 	);
 }
