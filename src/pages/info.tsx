d2d4b52e5706d47a1c78a0d75e31d48fbc9ee5d2
linre-90 <juho.lindemark@gmail.com>
2022-02-25 09:26:31 +0200

    Created spacer and big headline component

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -6,6 +6,8 @@ import {Cta} from "../components/projectComp/cta/callToAction";
 import {Footer} from "../components/projectComp/footer/footer";
 import {BrianBot} from "../components/external/brian/brian";
 import { CookieBanner } from "../components/projectComp/cookie/cookie";
+import {CommonSpecialHeadline} from "../components/projectComp/headline/commonSpecialHeader";
+import {Spacer} from "../components/projectComp/spacer/spacer";
 
 
 /**
@@ -22,20 +24,30 @@ const Info:React.FC = ():React.ReactElement => {
             <div className="container py-5 bg-opacity-75">
                 <TextSection 
                     text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis bibendum lorem sed dolor volutpat, eu ultrices nisi facilisis. Nam eget massa sed purus tempus imperdiet ac at ante. Nam et tristique leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut nunc libero, consectetur eu feugiat at, molestie id nisl. Mauris maximus et ex egestas vehicula. Vestibulum turpis velit, euismod ut lectus eget, tincidunt viverra velit. Mauris iaculis ullamcorper leo, in venenatis urna lacinia in."
-                    header="Who am i?"
+                    header=""
                     headerSize={1}
-                ></TextSection>
+                    flipChildOrder={true}
+                >
+                    <CommonSpecialHeadline header="Who am i"></CommonSpecialHeadline>
+                </TextSection>
+
+                <Spacer></Spacer>
+                
                 <TextSection
                     text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis bibendum lorem sed dolor volutpat, eu ultrices nisi facilisis. Nam eget massa sed purus tempus imperdiet ac at ante. Nam et tristique leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut nunc libero, consectetur eu feugiat at, molestie id nisl. Mauris maximus et ex egestas vehicula. Vestibulum turpis velit, euismod ut lectus eget, tincidunt viverra velit. Mauris iaculis ullamcorper leo, in venenatis urna lacinia in."
                     header="Education and motives"
-                    headerSize={2}            
+                    headerSize={2}       
                 >
-                    <Cta text="CV can be found here" url={process.env.CV_ADDRESS} ></Cta>         
+                    <a className="btn btn-danger btn-lg" href={process.env.CV_ADDRESS} ><b className="text-primary">CV can be found here</b></a>         
                 </TextSection>
+
+                <Spacer></Spacer>
+
                 <TextSection 
                     text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis bibendum lorem sed dolor volutpat, eu ultrices nisi facilisis. Nam eget massa sed purus tempus imperdiet ac at ante. Nam et tristique leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut nunc libero, consectetur eu feugiat at, molestie id nisl. Mauris maximus et ex egestas vehicula. Vestibulum turpis velit, euismod ut lectus eget, tincidunt viverra velit. Mauris iaculis ullamcorper leo, in venenatis urna lacinia in."
                     header="Little more about me"
                     headerSize={2}
+                    flipChildOrder={true}
                 ></TextSection>
                 <Footer></Footer>
                 <BrianBot></BrianBot>
