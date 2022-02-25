322bce799dead4d518b19efdb4e46fbd7c2ee1f4
linre-90 <juho.lindemark@gmail.com>
2022-02-25 10:11:20 +0200

    added info page animation

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
 import {NavMenu} from "../components/projectComp/navMenu/navMenu";
 import {getAddresses} from "../Addresses"
 import {TextSection} from "../components/projectComp/textSection/textSection";
-import {Cta} from "../components/projectComp/cta/callToAction";
 import {Footer} from "../components/projectComp/footer/footer";
 import {BrianBot} from "../components/external/brian/brian";
 import { CookieBanner } from "../components/projectComp/cookie/cookie";
@@ -16,39 +17,91 @@ import {Spacer} from "../components/projectComp/spacer/spacer";
  * @returns 
  */
 const Info:React.FC = ():React.ReactElement => {
+
+    const infoPageAnimationRef = useRef(null);
+    gsap.registerPlugin(ScrollTrigger);
+
+    useEffect(() => {
+        const element = infoPageAnimationRef.current;
+
+        gsap.to(element.querySelector("#box0"), {
+            x:"0", 
+            opacity:1, 
+            scrollTrigger:{
+                trigger: element.querySelector("#box0"),
+                start: "top center",
+                end: "bottom top",
+                markers: true
+            }
+        });
+
+        gsap.to(element.querySelector("#box1"), {
+            x:"0", 
+            opacity:1, 
+            scrollTrigger:{
+                trigger: element.querySelector("#box1"),
+                start: "top center",
+                end: "bottom top",
+                markers: true
+                
+            }
+        });
+
+        gsap.to(element.querySelector("#box2"), {
+            opacity:1, 
+            duration:1,
+            scrollTrigger:{
+                trigger: element.querySelector("#box2"),
+                start: "top center",
+                end: "bottom top",
+                markers: true
+            }
+        });
+    },[])
+
+
     return (
-        <div>
+        <div ref={infoPageAnimationRef}>
             <CookieBanner></CookieBanner>
 
             <NavMenu header="Info" innerHeader="Info" navLinks={getAddresses(2)}></NavMenu>
-            <div className="container py-5 bg-opacity-75">
-                <TextSection 
-                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis bibendum lorem sed dolor volutpat, eu ultrices nisi facilisis. Nam eget massa sed purus tempus imperdiet ac at ante. Nam et tristique leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut nunc libero, consectetur eu feugiat at, molestie id nisl. Mauris maximus et ex egestas vehicula. Vestibulum turpis velit, euismod ut lectus eget, tincidunt viverra velit. Mauris iaculis ullamcorper leo, in venenatis urna lacinia in."
-                    header=""
-                    headerSize={1}
-                    flipChildOrder={true}
-                >
-                    <CommonSpecialHeadline header="Who am i"></CommonSpecialHeadline>
-                </TextSection>
+            <div className="container py-5 bg-opacity-75 ">
 
-                <Spacer></Spacer>
-                
-                <TextSection
-                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis bibendum lorem sed dolor volutpat, eu ultrices nisi facilisis. Nam eget massa sed purus tempus imperdiet ac at ante. Nam et tristique leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut nunc libero, consectetur eu feugiat at, molestie id nisl. Mauris maximus et ex egestas vehicula. Vestibulum turpis velit, euismod ut lectus eget, tincidunt viverra velit. Mauris iaculis ullamcorper leo, in venenatis urna lacinia in."
-                    header="Education and motives"
-                    headerSize={2}       
-                >
-                    <a className="btn btn-danger btn-lg" href={process.env.CV_ADDRESS} ><b className="text-primary">CV can be found here</b></a>         
-                </TextSection>
+                <div id="box0" className="animationLeftPositionX">
+                    <TextSection 
+                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis bibendum lorem sed dolor volutpat, eu ultrices nisi facilisis. Nam eget massa sed purus tempus imperdiet ac at ante. Nam et tristique leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut nunc libero, consectetur eu feugiat at, molestie id nisl. Mauris maximus et ex egestas vehicula. Vestibulum turpis velit, euismod ut lectus eget, tincidunt viverra velit. Mauris iaculis ullamcorper leo, in venenatis urna lacinia in."
+                        header=""
+                        headerSize={1}
+                        flipChildOrder={true}
+                    >
+                        <CommonSpecialHeadline header="Who am i"></CommonSpecialHeadline>
+                    </TextSection>
+                </div>
 
                 <Spacer></Spacer>
 
-                <TextSection 
-                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis bibendum lorem sed dolor volutpat, eu ultrices nisi facilisis. Nam eget massa sed purus tempus imperdiet ac at ante. Nam et tristique leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut nunc libero, consectetur eu feugiat at, molestie id nisl. Mauris maximus et ex egestas vehicula. Vestibulum turpis velit, euismod ut lectus eget, tincidunt viverra velit. Mauris iaculis ullamcorper leo, in venenatis urna lacinia in."
-                    header="Little more about me"
-                    headerSize={2}
-                    flipChildOrder={true}
-                ></TextSection>
+                <div id="box1" className="animationrigthPositionX">
+                    <TextSection
+                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis bibendum lorem sed dolor volutpat, eu ultrices nisi facilisis. Nam eget massa sed purus tempus imperdiet ac at ante. Nam et tristique leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut nunc libero, consectetur eu feugiat at, molestie id nisl. Mauris maximus et ex egestas vehicula. Vestibulum turpis velit, euismod ut lectus eget, tincidunt viverra velit. Mauris iaculis ullamcorper leo, in venenatis urna lacinia in."
+                        header=""
+                        headerSize={2}     
+                        flipChildOrder={true}  
+                    >
+                        <CommonSpecialHeadline header="Education"></CommonSpecialHeadline>
+                        <a className="btn btn-danger btn-lg mb-4 mt-4" href={process.env.CV_ADDRESS} ><b className="text-primary">CV can be found here</b></a>         
+                    </TextSection>
+                </div>
+
+                <Spacer></Spacer>
+
+                <div id="box2" className="fromOpacityZero">
+                    <TextSection 
+                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis bibendum lorem sed dolor volutpat, eu ultrices nisi facilisis. Nam eget massa sed purus tempus imperdiet ac at ante. Nam et tristique leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut nunc libero, consectetur eu feugiat at, molestie id nisl. Mauris maximus et ex egestas vehicula. Vestibulum turpis velit, euismod ut lectus eget, tincidunt viverra velit. Mauris iaculis ullamcorper leo, in venenatis urna lacinia in."
+                        header="Little more about me"
+                        headerSize={2}
+                        flipChildOrder={true}
+                    ></TextSection>
+                </div>
                 <Footer></Footer>
                 <BrianBot></BrianBot>
             </div>
