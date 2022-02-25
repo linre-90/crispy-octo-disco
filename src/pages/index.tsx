bcca7065f83fb2d1f657b14eba6e3d75dae41f89
linre-90 <juho.lindemark@gmail.com>
2022-02-25 11:02:45 +0200

    index added text via graphql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,12 +8,29 @@ import { getAddresses } from "../Addresses";
 import {TextSection} from "../components/projectComp/textSection/textSection";
 import {CookieBanner} from "../components/projectComp/cookie/cookie";
 import { FrontPageSpecial } from "../components/projectComp/headline/frontPageSpecial";
+import { graphql, useStaticQuery } from "gatsby";
+
 
 /**
  * Index page markup
  * @returns react component
  */
 const IndexPage: React.FC = (): React.ReactElement => {
+	
+	const pageTexts = useStaticQuery(graphql`
+		query FrontPageTexts {
+			mainpageJson {
+				mainhead
+				textSection {
+					header
+					text
+				}
+				cta
+			}
+			
+	  	}
+    `);
+
 	return (
 		<div>
 			<NavMenu header="Navigation" innerHeader="Pages" navLinks={ getAddresses(0) }></NavMenu>
@@ -24,15 +41,14 @@ const IndexPage: React.FC = (): React.ReactElement => {
 					<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></script>
 				</Helmet>
 				<div className="row">
-					<FrontPageSpecial words={["Huge", "Font", "Cause", "I'm", "Hip"]}></FrontPageSpecial>
+					<FrontPageSpecial words={ pageTexts.mainpageJson.mainhead }></FrontPageSpecial>
 				</div>
 				<TextSection 
-					header="Welcome" 
-					text={`
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum orci ut nisl mattis egestas. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque volutpat tempus nibh, at aliquam turpis sodales id. Donec in urna ut magna sollicitudin mattis nec a massa. Integer massa enim, fermentum vel volutpat ac, dictum sit amet erat. Aliquam vitae rhoncus nunc. Ut eu nunc vitae odio consectetur efficitur. Morbi gravida at libero id volutpat. Aenean eu mauris auctor, blandit turpis ac, lobortis nunc. Donec dapibus leo lorem, in imperdiet felis convallis nec. Duis et egestas dolor. Nunc est dolor, rhoncus luctus tempus quis, aliquet sed lorem. In ac condimentum ligula. `}
+					header={ pageTexts.mainpageJson.textSection.header }
+					text={ pageTexts.mainpageJson.textSection.text }
 					headerSize={2}
 					>
-					<Cta text="Call to action" url="/"></Cta>
+					<Cta text={ pageTexts.mainpageJson.cta } url="/portfolio"></Cta>
 				</TextSection>
 				<BrianBot></BrianBot>
 				<Footer></Footer>
