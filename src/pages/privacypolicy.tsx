c64fc9171d6b8baaf596b40a29bd5e59e4f5aa00
linre-90 <juho.lindemark@gmail.com>
2022-02-25 12:44:22 +0200

    privacy box text graphql

--- src/pages/privacypolicy.tsx
+++ src/pages/privacypolicy.tsx
@@ -5,25 +5,42 @@ import {TextSection} from "../components/projectComp/textSection/textSection";
 import {Footer} from "../components/projectComp/footer/footer";
 import {BrianBot} from "../components/external/brian/brian";
 import { CookieBanner } from "../components/projectComp/cookie/cookie";
+import {useStaticQuery, graphql} from "gatsby";
 
 /**
  * Privacy policy page
  * @returns privacy policy page
  */
 const PrivacyPolicy = () => {
+    
+    const privacyTexts = useStaticQuery(graphql`
+        query privacyTexts {
+            privacyJson {
+                box0 {
+                    head
+                    text
+                }
+                box1 {
+                    head
+                    text
+                }
+            }
+        }
+    `);
+    
     return (
         <div>
             <CookieBanner></CookieBanner>
             <NavMenu header="Privacy policy" innerHeader="Privacy policy" navLinks={getAddresses(5)}></NavMenu>
             <div className="container py-5 bg-opacity-75">
                 <TextSection 
-                    text="Your data is not shared for third parties. I do not store, share or distribute your personal information for third parties. Any personal data like email address submitted in contact form is used only for communication and will not be used for marketing. Any information submitted through form is preserved only for lifespan of communication, after that data is permanently deleted."
-                    header="Privacy policy"
+                    text={ privacyTexts.privacyJson.box0.text }
+                    header={ privacyTexts.privacyJson.box0.head }
                     headerSize={1}
                 ></TextSection>
                 <TextSection 
-                    text="This site does not track users or visitors. Any cookies served are only stricktly necessary for site functionality. These cookies do not track users personal data. How ever i cannot remove previous cookies set from other sites and cannot be held responsible about those."
-                    header="Cookies"
+                    text= { privacyTexts.privacyJson.box1.text }
+                    header={ privacyTexts.privacyJson.box1.head }
                     headerSize={2}
                 ></TextSection>         
                 <TextSection 
